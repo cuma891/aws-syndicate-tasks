b89bf9aa30afdbbaa3f43b1b16fbde1aac601008
Uma_Chakrakody <uma_chakrakody@epam.com>
2024-04-15 17:35:14 +0530

    changes

--- task02/jsrc/main/java/com/helloworld/HelloWorld.java
+++ task02/jsrc/main/java/com/helloworld/HelloWorld.java
@@ -1,51 +1,54 @@
-package com.task02;
+package com.helloworld;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.Architecture;
-import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
-@LambdaHandler(
-		lambdaName = "hello_world",
+@LambdaHandler(lambdaName = "hello_world",
 		roleName = "hello_world-role",
-		layers = {"sdk-layer"},
+		isPublishVersion = true,
 		runtime = DeploymentRuntime.JAVA11,
 		architecture = Architecture.ARM64,
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-@LambdaLayer(
-		layerName = "sdk-layer",
-		libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
-		runtime = DeploymentRuntime.JAVA11,
-		architectures = {Architecture.ARM64},
-		artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+		String body = prepareResponse();
+		return new APIGatewayProxyResponseEvent().withBody(body).withStatusCode(200);
+	}
+
+	private String prepareResponse() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("statusCode", 200);
+		map.put("message", "Hello from Lambda");
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String response;
+		try {
+			response = objectMapper.writeValueAsString(map);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+
+		return response;
 	}
 }
